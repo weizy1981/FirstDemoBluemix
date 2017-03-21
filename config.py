bfefbd04ad5eff169250dc941d4a5709bde61d18
weizy1981 <1553291835@qq.com>
2017-03-21 16:29:13 +0800

    Update config.py
    Mark the password

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-bluemix_username = '214d23ca-5fd2-405b-bd69-fcd882c7cc97'
-bluemix_password = 'BruqqgUdid6i'
+bluemix_username = 'xxxxxxxxxxxxxxx
+bluemix_password = 'xxxxxxxxxxx'
 bluemix_watsonvoice = 'en-US_AllisonVoice'
